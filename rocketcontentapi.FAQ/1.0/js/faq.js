3c65a0ebdd56551c85bbc06e83c5fa42e54d29c6
leedavi <dcl@agence-sesame.fr>
2023-11-30 15:00:58 +0100

    FAQ Accordian

---
+++ rocketcontentapi.FAQ/1.0/js/faq.js
@@ -0,0 +1,15 @@
+﻿function slideAccordianFAQ(contentId, moduleId) {
+    if ($('.a-active' + moduleId).attr('id') == contentId) {
+        $('.a-active' + moduleId).slideUp();
+        $('.a-active' + moduleId).removeClass('accordion-active');
+        $('.a-active' + moduleId).removeClass('a-active' + moduleId);
+    }
+    else {
+        $('.a-active' + moduleId).slideUp();
+        $('.a-active' + moduleId).removeClass('accordion-active');
+        $('.a-active' + moduleId).removeClass('a-active' + moduleId);
+        $('#' + contentId).slideDown();
+        $('#' + contentId).addClass('accordion-active');
+        $('#' + contentId).addClass('a-active' + moduleId);
+    }
+}
