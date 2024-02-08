8f496e92c0578cb16289af8c36d6bec58326907b
FrankC <frank@nevoweb.com>
2024-02-08 11:56:54 +0100

    Add files via upload

---
+++ rocketcontentapi.FlexContentAccordion/1.0/js/accordion.js
@@ -0,0 +1,15 @@
+﻿function slideAccordionFC(contentId, moduleId) {
+    if ($('.fc-active' + moduleId).attr('id') == contentId) {
+        $('.fc-active' + moduleId).slideUp();
+        $('.fc-active' + moduleId).parent().removeClass('flexcontent-active');
+        $('.fc-active' + moduleId).removeClass('fc-active' + moduleId);
+    }
+    else {
+        $('.fc-active' + moduleId).slideUp();
+        $('.fc-active' + moduleId).parent().removeClass('flexcontent-active');
+        $('.fc-active' + moduleId).removeClass('fc-active' + moduleId);
+        $('#' + contentId).slideDown();
+        $('#' + contentId).parent().addClass('flexcontent-active');
+        $('#' + contentId).addClass('fc-active' + moduleId);
+    }
+}
